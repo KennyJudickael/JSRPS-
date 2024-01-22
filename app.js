e55084af03936647ff88f47e5231cd09276f7242
RAVOTINA Kenny Judickael <judickaelkenny@gmail.com>
2024-01-22 22:26:01 +0300

    choix au hasard ordinateur

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const userChoiceDisplay = document.getElementById("user-choice");
 const resultDisplay = document.getElementById("result");
 
 let userChoice;
+let computerChoice;
 
 const possibleChoises = document.querySelectorAll("button");
 
@@ -10,5 +11,24 @@ possibleChoises.forEach((possibleChoises) =>
   possibleChoises.addEventListener("click", (e) => {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
+
+    generateComputerChoice();
   })
 );
+
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3) + 1; // on peut aussi utilsier possibleChoices.length
+  //   console.log(randomNumber);
+
+  if (randomNumber === 1) {
+    computerChoice = "Pierre";
+  }
+  if (randomNumber === 2) {
+    computerChoice = "Papier";
+  }
+  if (randomNumber === 3) {
+    computerChoice = "Ciseaux";
+  }
+
+  computerChoiceDisplay.innerHTML = computerChoice;
+}
